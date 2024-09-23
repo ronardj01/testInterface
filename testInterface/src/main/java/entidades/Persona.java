e6b890fdff597aa99974a732043baed2385580fb
ronardj01 <ronardj01@gmail.com>
2024-09-23 15:10:13 +0200

    create generarDNI method in Persona class

--- testInterface/src/main/java/entidades/Persona.java
+++ testInterface/src/main/java/entidades/Persona.java
@@ -24,6 +24,7 @@ public class Persona {
         this.edad = 0;
         this.peso = 0;
         this.altura = 0;
+        this.DNI = this.generaDNI();
     }
 
     public Persona(String nombre, char sexo, int edad) {
@@ -32,6 +33,7 @@ public class Persona {
         this.edad = edad;
         this.peso = 0;
         this.altura = 0;
+        this.DNI = this.generaDNI();
     }
 
     public Persona(String nombre, String DNI, char sexo, int edad, float peso, float altura) {
@@ -101,7 +103,7 @@ public class Persona {
 
     public int calcularIMC() {
         int resultado;
-        double imc = this.peso / Math.pow(this.altura,2);
+        double imc = this.peso / Math.pow(this.altura, 2);
 
         if (imc < 18.5) {
             resultado = -1;
@@ -122,12 +124,81 @@ public class Persona {
 
         return resultado;
     }
-    
+
     private char comprobarSexo(char sexo) {
         char resultado = Persona.SEXO;
-        if(sexo == 'H' || sexo == 'M') {
+        if (sexo == 'H' || sexo == 'M') {
             resultado = sexo;
         }
         return resultado;
+    }
+
+    private String generaDNI() {
+        String stringDNI = "";
+
+        // Generar numeros aleatorios de 8 digitos en un String (cada digito 0-9)
+        for (int i = 0; i < 8; i++) {
+            stringDNI += (int) ((Math.random() * 10) + 1) - 1;
+        }
+
+        // Calcular la letra del DNI con el modulo 23
+        int modulo = Integer.parseInt(stringDNI) % 23;
+
+        char letra = ' ';
+        switch (modulo) {
+            case 0 ->
+                letra = 'T';
+            case 1 ->
+                letra = 'R';
+            case 2 ->
+                letra = 'W';
+            case 3 ->
+                letra = 'A';
+            case 4 ->
+                letra = 'G';
+            case 5 ->
+                letra = 'M';
+            case 6 ->
+                letra = 'Y';
+            case 7 ->
+                letra = 'F';
+            case 8 ->
+                letra = 'P';
+            case 9 ->
+                letra = 'D';
+            case 10 ->
+                letra = 'X';
+            case 11 ->
+                letra = 'B';
+            case 12 ->
+                letra = 'N';
+            case 13 ->
+                letra = 'J';
+            case 14 ->
+                letra = 'Z';
+            case 15 ->
+                letra = 'S';
+            case 16 ->
+                letra = 'Q';
+            case 17 ->
+                letra = 'V';
+            case 18 ->
+                letra = 'H';
+            case 19 ->
+                letra = 'L';
+            case 20 ->
+                letra = 'C';
+            case 21 ->
+                letra = 'K';
+            case 22 ->
+                letra = 'E';
+            default ->
+                System.out.println("Error: Resto fuera de rango.");
+        }
+
+        //concatenar StringDNI con letra valida
+        stringDNI += letra;
+
+        return stringDNI;
     }
 }
