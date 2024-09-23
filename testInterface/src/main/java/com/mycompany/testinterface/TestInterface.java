542ee8a6e535598e9fde55a73893e3ee46a6f2a2
ronardj01 <ronardj01@gmail.com>
2024-09-23 12:51:42 +0200

    create a maven project

---
+++ testInterface/src/main/java/com/mycompany/testinterface/TestInterface.java
@@ -0,0 +1,12 @@
+package com.mycompany.testinterface;
+
+/**
+ *
+ * @author ronar
+ */
+public class TestInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
